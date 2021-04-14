2706b954d59a2e6eb9dd39aaad1fe192c8529478
Nitin Gokhale <nitin.r.gokhale@gmail.com>
2021-04-14 19:00:04 -0400

    Add files via upload

---
+++ nrg.mm
@@ -0,0 +1,150 @@
+<map version="freeplane 1.8.0">
+  <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+  <node TEXT="new mindmap" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1462853497" CREATED="1618421711776" MODIFIED="1618439224860"><hook NAME="MapStyle">
+      <properties show_icon_for_attributes="true" fit_to_viewport="false" show_note_icons="true" edgeColorConfiguration="#888888,#DCD9F2,#E6E6E6,#D9EAF2,#D9F2EF,#D9F2DD,#F0F2D9,#F2E4D9,#F2D9DD,#F2D9EE"/>
+      <map_styles>
+        <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt" TEXT_SHORTENED="true" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+          <font SIZE="24" BOLD="true"/>
+          <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+            <font SIZE="9"/>
+            <stylenode LOCALIZED_TEXT="default" COLOR="#202020" STYLE="bubble" TEXT_ALIGN="LEFT" BORDER_WIDTH="2.0 px">
+              <font NAME="Calibri" SIZE="8" BOLD="false" ITALIC="false"/>
+              <edge STYLE="bezier" WIDTH="2"/>
+            </stylenode>
+            <stylenode LOCALIZED_TEXT="defaultstyle.details">
+              <font SIZE="11"/>
+            </stylenode>
+            <stylenode LOCALIZED_TEXT="defaultstyle.attributes" BACKGROUND_COLOR="#cccccc" VGAP_QUANTITY="5.0 pt">
+              <font SIZE="9"/>
+            </stylenode>
+            <stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#767eea" BACKGROUND_COLOR="#ffffff"/>
+            <stylenode LOCALIZED_TEXT="defaultstyle.floating">
+              <edge STYLE="hide_edge"/>
+            </stylenode>
+          </stylenode>
+          <stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+            <font SIZE="9"/>
+            <stylenode LOCALIZED_TEXT="styles.important">
+              <icon BUILTIN="yes"/>
+              <edge COLOR="#0000cc"/>
+            </stylenode>
+          </stylenode>
+          <stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+            <font SIZE="9"/>
+            <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" BACKGROUND_COLOR="#cccccc" STYLE="bubble" VGAP_QUANTITY="20.0 pt" BORDER_WIDTH="4.0 pt">
+              <font SIZE="18" BOLD="true"/>
+              <edge WIDTH="8"/>
+            </stylenode>
+            <stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" VGAP_QUANTITY="16.0 pt" BORDER_WIDTH="4.0 px" MAX_WIDTH="160.0 pt" MIN_WIDTH="160.0 pt">
+              <font SIZE="16" BOLD="false"/>
+              <edge WIDTH="6"/>
+            </stylenode>
+            <stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" VGAP_QUANTITY="12.0 pt" BORDER_WIDTH="3.2 px" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+              <font SIZE="14" BOLD="false"/>
+              <edge WIDTH="4"/>
+            </stylenode>
+            <stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" VGAP_QUANTITY="8.0 pt" BORDER_WIDTH="2.4 px" MAX_WIDTH="100.0 pt" MIN_WIDTH="100.0 pt">
+              <font SIZE="12" BOLD="false"/>
+            </stylenode>
+            <stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" VGAP_QUANTITY="4.0 pt" BORDER_WIDTH="2.4 px" MAX_WIDTH="80.0 pt" MIN_WIDTH="80.0 pt">
+              <font SIZE="12" BOLD="false"/>
+            </stylenode>
+            <stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+              <font SIZE="11" BOLD="false"/>
+            </stylenode>
+            <stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+              <font SIZE="10" BOLD="false"/>
+            </stylenode>
+          </stylenode>
+        </stylenode>
+      </map_styles>
+    </hook>
+    <hook NAME="AutomaticEdgeColor" COUNTER="19" RULE="ON_BRANCH_CREATION"/>
+    <edge COLOR="#cccccc"/>
+    <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+    <node TEXT="Colors" POSITION="left" ID="ID_1811525136" CREATED="1618439659394" MODIFIED="1618439732850" BACKGROUND_COLOR="#e6e6e6">
+      <edge COLOR="#e6e6e6"/>
+      <node TEXT="#C9C9C9" ID="ID_1119311329" CREATED="1618435729069" MODIFIED="1618439768735" BACKGROUND_COLOR="#c9c9c9">
+        <edge COLOR="#c9c9c9"/>
+        <node TEXT="#D9D9D9" ID="ID_1724549032" CREATED="1618435733939" MODIFIED="1618439204998" BACKGROUND_COLOR="#d9d9d9">
+          <edge COLOR="#d9d9d9"/>
+          <node TEXT="#E6E6E6" ID="ID_319025387" CREATED="1618435734940" MODIFIED="1618439199474" BACKGROUND_COLOR="#e6e6e6">
+            <edge COLOR="#e6e6e6"/>
+          </node>
+        </node>
+      </node>
+      <node TEXT="#BBB5DE" ID="ID_1223469789" CREATED="1618435729069" MODIFIED="1618439777131" BACKGROUND_COLOR="#bbb5de">
+        <edge COLOR="#bbb5de"/>
+        <node TEXT="#C8C1F0" ID="ID_1678412407" CREATED="1618435733939" MODIFIED="1618439189260" BACKGROUND_COLOR="#c8c1f0">
+          <edge COLOR="#c8c1f0"/>
+          <node TEXT="#DCD9F2" ID="ID_54562689" CREATED="1618435734940" MODIFIED="1618439194214" BACKGROUND_COLOR="#dcd9f2">
+            <edge COLOR="#dcd9f2"/>
+          </node>
+        </node>
+      </node>
+      <node TEXT="#B5D0DE" ID="ID_684645137" CREATED="1618435729069" MODIFIED="1618439800891" BACKGROUND_COLOR="#b5d0de">
+        <edge COLOR="#b5d0de"/>
+        <node TEXT="#C1E1F0" ID="ID_617382868" CREATED="1618435733939" MODIFIED="1618439165023" BACKGROUND_COLOR="#c1e1f0">
+          <edge COLOR="#c1e1f0"/>
+          <node TEXT="#D9EAF2" ID="ID_1664711774" CREATED="1618435734940" MODIFIED="1618439169962" BACKGROUND_COLOR="#d9eaf2">
+            <edge COLOR="#d9eaf2"/>
+          </node>
+        </node>
+      </node>
+      <node TEXT="#B5DED8" ID="ID_1383918161" CREATED="1618435729069" MODIFIED="1618439805642" BACKGROUND_COLOR="#b5ded8">
+        <edge COLOR="#b5ded8"/>
+        <node TEXT="#C1F0EA" ID="ID_767047235" CREATED="1618435733939" MODIFIED="1618439147548" BACKGROUND_COLOR="#c1f0ea">
+          <edge COLOR="#c1f0ea"/>
+          <node TEXT="#D9F2EF" ID="ID_1271653787" CREATED="1618435734940" MODIFIED="1618439142803" BACKGROUND_COLOR="#d9f2ef">
+            <edge COLOR="#d9f2ef"/>
+          </node>
+        </node>
+      </node>
+      <node TEXT="#B5DEBB" ID="ID_838823094" CREATED="1618435729069" MODIFIED="1618439830878" BACKGROUND_COLOR="#b5debb">
+        <edge COLOR="#b5debb"/>
+        <node TEXT="#C1F0C8" ID="ID_1188631886" CREATED="1618435733939" MODIFIED="1618439132340" BACKGROUND_COLOR="#c1f0c8">
+          <edge COLOR="#c1f0c8"/>
+          <node TEXT="#D9F2DD" ID="ID_1109846245" CREATED="1618435734940" MODIFIED="1618439137109" BACKGROUND_COLOR="#d9f2dd">
+            <edge COLOR="#d9f2dd"/>
+          </node>
+        </node>
+      </node>
+      <node TEXT="#DADEB5" ID="ID_806059256" CREATED="1618435729069" MODIFIED="1618439835618" BACKGROUND_COLOR="#dadeb5">
+        <edge COLOR="#dadeb5"/>
+        <node TEXT="#ECF0C1" ID="ID_774193144" CREATED="1618435733939" MODIFIED="1618439112170" BACKGROUND_COLOR="#ecf0c1">
+          <edge COLOR="#ecf0c1"/>
+          <node TEXT="#F0F2D9" ID="ID_758189489" CREATED="1618435734940" MODIFIED="1618439106830" BACKGROUND_COLOR="#f0f2d9">
+            <edge COLOR="#f0f2d9"/>
+          </node>
+        </node>
+      </node>
+      <node TEXT="#DEC7B5" ID="ID_1292659644" CREATED="1618435729069" MODIFIED="1618439854230" BACKGROUND_COLOR="#dec7b5">
+        <edge COLOR="#dec7b5"/>
+        <node TEXT="#F0D6C1" ID="ID_1119040770" CREATED="1618435733939" MODIFIED="1618439095083" BACKGROUND_COLOR="#f0d6c1">
+          <edge COLOR="#f0d6c1"/>
+          <node TEXT="#F2E4D9" ID="ID_720713196" CREATED="1618435734940" MODIFIED="1618439100288" BACKGROUND_COLOR="#f2e4d9">
+            <edge COLOR="#f2e4d9"/>
+          </node>
+        </node>
+      </node>
+      <node TEXT="#DEB5BB" ID="ID_1534146980" CREATED="1618435729069" MODIFIED="1618439859454" BACKGROUND_COLOR="#deb5bb">
+        <edge COLOR="#deb5bb"/>
+        <node TEXT="#F0C1C8" ID="ID_509480548" CREATED="1618435733939" MODIFIED="1618438915169" BACKGROUND_COLOR="#f0c1c8">
+          <edge COLOR="#f0c1c8"/>
+          <node TEXT="#F2D9DD" ID="ID_596427321" CREATED="1618435734940" MODIFIED="1618438908327" BACKGROUND_COLOR="#f2d9dd">
+            <edge COLOR="#f2d9dd"/>
+          </node>
+        </node>
+      </node>
+      <node TEXT="#DEB5D8" ID="ID_282157772" CREATED="1618435729069" MODIFIED="1618439895241" BACKGROUND_COLOR="#deb5d8">
+        <edge COLOR="#deb5d8"/>
+        <node TEXT="#F0C1E9" ID="ID_1489809235" CREATED="1618435733939" MODIFIED="1618438892957" BACKGROUND_COLOR="#f0c1e9">
+          <edge COLOR="#f0c1e9"/>
+          <node TEXT="#F2D9EE" ID="ID_1326620012" CREATED="1618435734940" MODIFIED="1618438901634" BACKGROUND_COLOR="#f2d9ee">
+            <edge COLOR="#f2d9ee"/>
+          </node>
+        </node>
+      </node>
+    </node>
+  </node>
+</map>
