00306229a7dfc0aa184fa188874dceff0fffae71
Nitin Gokhale <nitin.r.gokhale@gmail.com>
2025-05-17 18:24:59 -0400

    Delete BackgroundColor.groovy

--- BackgroundColor.groovy
+++
@@ -1,6 +0,0 @@
-// @ExecutionModes({ON_SELECTED_NODE})
-node.style.setBackgroundColorCode(node.style.getEdge().getColorCode());
-
-// node.style.backgroundColorCode = node.style.getEdge().getColorCode();
-// node.style.setBackgroundColor(node.style.getEdge().getColor());
-
